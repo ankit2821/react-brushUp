08dc0c365230f4f1074f8fedc1ee47c061fbb774
ankit <you@example.com>
2024-07-15 00:27:07 +0530

    created card component

--- basic_react/src/App.jsx
+++ basic_react/src/App.jsx
@@ -1,9 +1,15 @@
 import Card from "./components/Card";
 import Counter from "./components/Counter";
 function App() {
+  const info = {
+    name: "Fido",
+    cap: "My dog's love is like a warm hug every day.",
+    places: ["Pune", "Ahemdabad", "Gadchiroli"],
+    btnText: "Read about me",
+  };
   return (
     <>
-      <Card />
+      <Card details={info} />
     </>
   );
 }
--- basic_react/src/components/Card.jsx
+++ basic_react/src/components/Card.jsx
@@ -6,13 +6,13 @@ function Card(props) {
     <>
       <div className="w-[300px] rounded-md border">
         <img
-          src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+          src="https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?auto=compress&cs=tinysrgb&w=600"
           alt="Laptop"
           className="h-[200px] w-full rounded-t-md object-cover"
         />
         <div className="p-4">
           <h1 className="inline-flex items-center text-lg font-semibold text-white">
-            About Macbook  {" "}
+            {props.details.name} {" "}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -29,26 +29,23 @@ function Card(props) {
               <polyline points="7 7 17 7 17 17"></polyline>
             </svg>
           </h1>
-          <p className="mt-3 text-sm text-gray-600">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-            debitis?
-          </p>
+          <p className="mt-3 text-sm text-gray-600">{props.details.cap}</p>
           <div className="mt-4">
             <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-              #Macbook
+              {props.details.places[0]}
             </span>
             <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-              #Apple
+              {props.details.places[1]}
             </span>
             <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-              #Laptop
+              {props.details.places[2]}
             </span>
           </div>
           <button
             type="button"
             className="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
           >
-            Read
+            {props.details.btnText}
           </button>
         </div>
       </div>
