1ecaab185e747daaaf42c78862153696bb86b1ee
ankit <you@example.com>
2024-07-15 00:01:05 +0530

    created card object

--- basic_react/src/App.jsx
+++ basic_react/src/App.jsx
@@ -1,10 +1,9 @@
+import Card from "./components/Card";
 import Counter from "./components/Counter";
 function App() {
   return (
     <>
-      <h1 className="text-7xl text-white bg-black font-bold underline">
-        Hello world!
-      </h1>
+      <Card />
     </>
   );
 }
